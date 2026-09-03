78f0ec3d332878cb5fe002ecf17f5b845399cf29
Diego <diegosoaresyt@gmail.com>
2026-09-03 17:08:16 -0300

    configirando keycloack na service

--- front/apps/webApp/src/app/core/autenticacao.service.ts
+++ front/apps/webApp/src/app/core/autenticacao.service.ts
@@ -1,6 +1,19 @@
+import Keycloak, { KeycloakInitOptions } from 'keycloak-js';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class AutenticacaoService {
+  private readonly instanciaKeycloak: Keycloak = new Keycloak({
+    url: 'http://localhost:8180',
+    realm: 'get-matriculas',
+    clientId: 'get-matriculas-front',
+  });
 
+  public inicializarKeyCloak(): Promise<boolean> {
+    const opcoes: KeycloakInitOptions = {
+      onLoad: 'login-required',
+      checkLoginIframe: false,
+    };
+    return this.instanciaKeycloak.init(opcoes);
+  }
 }
