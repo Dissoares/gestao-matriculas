ab6a0047321ff45f74e29289eb36f662c86c4726
Diego <diegosoaresyt@gmail.com>
2026-09-03 23:08:09 -0300

    adicionando rota login

--- front/apps/webApp/src/app/app.routes.ts
+++ front/apps/webApp/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login.component').then((m) => m.LoginComponent),
+  },
+
+  { path: '**', redirectTo: 'login' },
+];
