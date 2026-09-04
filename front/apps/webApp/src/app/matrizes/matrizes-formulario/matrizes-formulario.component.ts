6334db25641940eca32862f2ff59d89f22f63282
Diego <diegosoaresyt@gmail.com>
2026-09-03 22:25:00 -0300

    adicionando modulo matrizes

---
+++ front/apps/webApp/src/app/matrizes/matrizes-formulario/matrizes-formulario.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-matrizes-formulario',
+  templateUrl: './matrizes-formulario.component.html',
+  styleUrls: ['./matrizes-formulario.component.scss'],
+})
+export class MatrizesFormularioComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ front/apps/webApp/src/app/matrizes/matrizes-listagem/matrizes-listagem.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-matrizes-listagem',
+  templateUrl: './matrizes-listagem.component.html',
+  styleUrls: ['./matrizes-listagem.component.scss'],
+})
+export class MatrizesListagemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
