8af794b4117ac605b08e7cdf3cae68e46a37b301
Diego <diegosoaresyt@gmail.com>
2026-09-05 01:30:45 -0300

    removendo componentes sem utilidade

--- front/apps/webApp/src/app/modules/aluno/matriculas/formulario/formulario.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  standalone: true,
-  selector: 'app-matricula-formulario',
-  template: '<p>Nova Matrícula</p>',
-  imports: [],
-})
-export class FormularioComponent {}
